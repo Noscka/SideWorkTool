f6983978fab19ee04b9f8862d0bb83909d25f8ce
Ruski1 <adam882200@gmail.com>
2022-02-26 19:10:26 +0000

    Adding keyboard hooks

--- SWT/SWT.cpp
+++ SWT/SWT.cpp
@@ -4,9 +4,67 @@
 #include <Windows.h>
 #include <WinUser.h>
 
+HHOOK _hook;
+
+// This struct contains the data received by the hook callback. As you see in the callback function
+// it contains the thing you will need: vkCode = virtual key code.
+KBDLLHOOKSTRUCT kbdStruct;
+
+LRESULT __stdcall HookCallback(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	if (nCode >= 0)
+	{
+		// the action is valid: HC_ACTION.
+		if (wParam == WM_KEYDOWN)
+		{
+			// lParam is the pointer to the struct containing the data needed, so cast and assign it to kdbStruct.
+			kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
+			// a key (non-system) is pressed.
+			switch(kbdStruct.vkCode)
+			{
+			case VK_F2:
+				// F1 is pressed!
+				printf("F1 is pressed!");
+				break;
+
+			case 0x44:
+				printf("D");
+				break;
+			}
+		}
+	}
+
+	// call the next hook in the hook chain. This is nessecary or your hook chain will break and the hook stops
+	return CallNextHookEx(_hook, nCode, wParam, lParam);
+}
+
+void SetHook()
+{
+	// Set the hook and set it to use the callback function above
+	// WH_KEYBOARD_LL means it will set a low level keyboard hook. More information about it at MSDN.
+	// The last 2 parameters are NULL, 0 because the callback function is in the same thread and window as the
+	// function that sets and releases the hook. If you create a hack you will not need the callback function 
+	// in another place then your own code file anyway. Read more about it at MSDN.
+	if (!(_hook = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallback, NULL, 0)))
+	{
+		printf("Failed to install hook!");
+	}
+}
+
+void ReleaseHook()
+{
+	UnhookWindowsHookEx(_hook);
+}
+
 int main()
 {
-	enum { ONE_KEYID = 1 };
+	// Set the hook
+	SetHook();
+	
+	enum
+	{ 
+		ONE_KEYID = 1
+	};
 	RegisterHotKey(0, ONE_KEYID, MOD_SHIFT | MOD_CONTROL | MOD_ALT, 'K'); // register 1 key as hotkey
 	MSG msg;
 	while(GetMessage(&msg, 0, 0, 0))
