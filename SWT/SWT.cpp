3711616d2cfd1bf7765db1ae242643c1411d2a40
Ruski1 <adam882200@gmail.com>
2022-02-27 16:25:47 +0000

    Wide Update

--- SWT/SWT.cpp
+++ SWT/SWT.cpp
@@ -4,32 +4,85 @@
 #include <Windows.h>
 #include <WinUser.h>
 
+bool Enabled = true;
+
+// Hook Variable
 HHOOK _hook;
 
 // This struct contains the data received by the hook callback. As you see in the callback function
 // it contains the thing you will need: vkCode = virtual key code.
 KBDLLHOOKSTRUCT kbdStruct;
 
-LRESULT __stdcall HookCallback(int nCode, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK HookCallback(int nCode, WPARAM wParam, LPARAM lParam)
 {
-	if (nCode >= 0)
+	if(Enabled)
 	{
-		// the action is valid: HC_ACTION.
-		if (wParam == WM_KEYDOWN)
+		if(nCode >= 0)
 		{
-			// lParam is the pointer to the struct containing the data needed, so cast and assign it to kdbStruct.
-			kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
-			// a key (non-system) is pressed.
-			switch(kbdStruct.vkCode)
+			switch(wParam)
 			{
-			case VK_F2:
-				// F1 is pressed!
-				printf("F1 is pressed!");
-				break;
+			case WM_KEYDOWN:
+				// lParam is the pointer to the struct containing the data needed, so cast and assign it to kdbStruct.
+				kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
 
-			case 0x44:
-				printf("D");
-				break;
+				// KeyBoardState BYTE array
+				BYTE KeyboardState[256] = {};
+
+				// UnicodeCharacter output from ToUnicodeEx
+				wchar_t UnicodeCharacter[5] = {};
+
+				// Get keystate from Shift and alt when getting keyboardstate
+				GetKeyState(VK_SHIFT);
+				GetKeyState(VK_MENU);
+				GetKeyboardState(KeyboardState);
+
+				// Get the key hit while taking into account the modifiers (shift+/ -> ?)
+				ToUnicodeEx((UINT)kbdStruct.vkCode, (UINT)kbdStruct.scanCode, KeyboardState, UnicodeCharacter, sizeof(UnicodeCharacter) / sizeof(*UnicodeCharacter) - 1, (UINT)kbdStruct.flags, GetKeyboardLayout(0));
+
+				switch(UnicodeCharacter[0])
+				{
+				case 48: // 0
+				case 49: // 1
+				case 50: // 2
+				case 51: // 3
+				case 52: // 4
+				case 53: // 5
+				case 54: // 6
+				case 55: // 7
+				case 56: // 8
+				case 57: // 9
+					std::wcout << UnicodeCharacter << '\n';
+					return -1;
+
+				case 8: // {BACKSPACE}
+					printf("{Backspace}\n");
+					break;
+
+				case 13: // {ENTER}
+					printf("{Enter}\n");
+					break;
+
+				case 47: // /
+					printf("/ - {DIVIDE}\n");
+					break;
+				case 43: // +
+					printf("+ - {PLUS}\n");
+					break;
+				case 45: // -
+					printf("- - {MINUS}\n");
+					break;
+				case 42: // *
+					printf("* - {MULTIPLY}\n");
+					break;
+				case 61: // =
+					printf("= - {EQUALS}\n");
+					break;
+
+				default:
+					std::wcout << "ToUnicodeEx: " << UnicodeCharacter << '\n';
+					std::wcout << "ToUnicodeEx INT: " << (int)UnicodeCharacter[0] << '\n';
+					break;
+				}
 			}
 		}
 	}
@@ -63,9 +116,9 @@ int main()
 	
 	enum
 	{ 
-		ONE_KEYID = 1
+		KEYID = 1 // Ctrl+Shift+alt+k key ID
 	};
-	RegisterHotKey(0, ONE_KEYID, MOD_SHIFT | MOD_CONTROL | MOD_ALT, 'K'); // register 1 key as hotkey
+	RegisterHotKey(0, KEYID, MOD_SHIFT | MOD_CONTROL | MOD_ALT, 'K'); // register 1 key as hotkey
 	MSG msg;
 	while(GetMessage(&msg, 0, 0, 0))
 	{
@@ -73,9 +126,18 @@ int main()
 		switch(msg.message)
 		{
 		case WM_HOTKEY:
-			if(msg.wParam == ONE_KEYID)
+			if(msg.wParam == KEYID)
 			{
-				printf("1 Pressed\n");
+				Enabled = !Enabled;
+
+				if(Enabled)
+				{
+					printf("Enabled\n");
+				}
+				else
+				{
+					printf("Disabled\n");
+				}
 			}
 		}
 	}
