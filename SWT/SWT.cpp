28e16610dec828b6dc52dff4d9209b176d4e4f27
Ruski1 <adam882200@gmail.com>
2022-03-06 16:04:48 +0000

    Fix

--- SWT/SWT.cpp
+++ SWT/SWT.cpp
@@ -202,34 +202,6 @@ void ReleaseHook()
 
 int main()
 {
-	std::this_thread::sleep_for(std::chrono::seconds(5));
-
-	POINT pPoint{};
-	DWORD dwCurrentID, dwCaretID;
-	HWND hCaret = ::GetForegroundWindow();
-	if(hCaret)
-	{
-		dwCaretID = GetWindowThreadProcessId(hCaret, NULL);
-		dwCurrentID = GetCurrentThreadId();
-		if(dwCurrentID != dwCaretID)
-		{
-			if(AttachThreadInput(dwCurrentID, dwCaretID, TRUE))
-			{
-				::SetFocus(hCaret);
-				::GetCaretPos(&pPoint);
-
-			}
-			else
-			{
-				//report something here
-			}
-		}
-
-	}
-	std::cout << "(" << pPoint.x << ", " << pPoint.y << ")";
-	AttachThreadInput(dwCurrentID, dwCaretID, FALSE);
-	return 0;
-
 	// Set the hook
 	SetHook();
 
