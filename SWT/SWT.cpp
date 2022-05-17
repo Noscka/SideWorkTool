3b404e044cd77594a53c9b4791e2f5538a97d269
Ruski1 <adam882200@gmail.com>
2022-05-17 13:35:28 +0100

    Function to get the Unicode character pressed from lParam

--- SWT/SWT.cpp
+++ SWT/SWT.cpp
@@ -9,8 +9,9 @@
 
 #include "./TinyExpr/tinyexpr.h"
 
-// If Program is enabled
-bool Enabled = false;
+// If Equation Calculation is enabled
+bool EnabledEquation = false;
+bool EnableAutoSelect = false, AutoSelectFirstPass = false;
 
 // Equation Char Array
 char Equation[124] = {};
@@ -77,121 +78,169 @@ bool isNumber(const std::string& str)
 	}
 	return true;
 }
+
+wchar_t* GetUnicodeCharacter(LPARAM lParam)
+{
+	kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
+
+			// KeyBoardState BYTE array
+	BYTE KeyboardState[256] = {};
+
+	// UnicodeCharacter output from ToUnicodeEx
+	wchar_t UnicodeCharacter[3] = {};
+
+	// Get keystate from Shift and alt when getting keyboardstate
+	GetKeyState(VK_SHIFT);
+	GetKeyState(VK_MENU);
+	GetKeyboardState(KeyboardState);
+
+	// Get the key hit while taking into account the modifiers (shift+5 -> %)
+	ToUnicodeEx((UINT)kbdStruct.vkCode, 
+				(UINT)kbdStruct.scanCode, 
+				KeyboardState, UnicodeCharacter, 
+				sizeof(UnicodeCharacter) / sizeof(*UnicodeCharacter) - 1, 
+				(UINT)kbdStruct.flags, 
+				GetKeyboardLayout(0));
+
+	return UnicodeCharacter;
+}
 #pragma endregion
 
 LRESULT CALLBACK HookCallback(int nCode, WPARAM wParam, LPARAM lParam)
 {
-	if(Enabled)
+	if (nCode < 0)
 	{
-		if(nCode >= 0)
+		return CallNextHookEx(_hook, nCode, wParam, lParam);
+	}
+
+	if(EnabledEquation)
+	{
+		switch(wParam)
 		{
-			switch(wParam)
+		// if keydown hook event (allows for holding and "spamming" key)
+		case WM_KEYDOWN:
+			//// Pointer to UnicodeCharacter Array
+			wchar_t UnicodeCharacter[3] = {};
+			*UnicodeCharacter = *GetUnicodeCharacter(lParam);
+
+			// Coord for backspace cursor position editing
+			COORD NewCoord;
+
+			switch(UnicodeCharacter[0])
 			{
-			// if keydown hook event (allows for holding and "spamming" key)
-			case WM_KEYDOWN:
-				// lParam is the pointer to the struct containing the data needed, so cast and assign it to kdbStruct.
-				kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
+			case 48: // 0
+			case 49: // 1
+			case 50: // 2
+			case 51: // 3
+			case 52: // 4
+			case 53: // 5
+			case 54: // 6
+			case 55: // 7
+			case 56: // 8
+			case 57: // 9
+				std::wcout << UnicodeCharacter;
+				AddToEquation(UnicodeCharacter[0]);
+				return -1;
 
-				// KeyBoardState BYTE array
-				BYTE KeyboardState[256] = {};
-
-				// UnicodeCharacter output from ToUnicodeEx
-				wchar_t UnicodeCharacter[3] = {};
-
-				// Get keystate from Shift and alt when getting keyboardstate
-				GetKeyState(VK_SHIFT);
-				GetKeyState(VK_MENU);
-				GetKeyboardState(KeyboardState);
-
-				// Get the key hit while taking into account the modifiers (shift+5 -> %)
-				ToUnicodeEx((UINT)kbdStruct.vkCode, (UINT)kbdStruct.scanCode, KeyboardState, UnicodeCharacter, sizeof(UnicodeCharacter) / sizeof(*UnicodeCharacter) - 1, (UINT)kbdStruct.flags, GetKeyboardLayout(0));
-
-				// Coord for backspace cursor position editing
-				COORD NewCoord;
-
-				switch(UnicodeCharacter[0])
+			case 8: // {BACKSPACE}
+				if(EquationArrayIndexPointer > 0)
 				{
-				case 48: // 0
-				case 49: // 1
-				case 50: // 2
-				case 51: // 3
-				case 52: // 4
-				case 53: // 5
-				case 54: // 6
-				case 55: // 7
-				case 56: // 8
-				case 57: // 9
-					std::wcout << UnicodeCharacter;
-					AddToEquation(UnicodeCharacter[0]);
-					return -1;
-
-				case 8: // {BACKSPACE}
-					if(EquationArrayIndexPointer > 0)
-					{
-						NewCoord = {(SHORT)(GetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE)).X - 1), GetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE)).Y}; // create new coord with x-1 and same y
-						SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), NewCoord); // use new coord
-						printf(" "); // delete character
-						SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), NewCoord); // go back 1 space
-						EquationArrayIndexPointer--;
-					}
-
-					Equation[EquationArrayIndexPointer] = NULL;
-					return -1;
-
-				case 47: // /
-					printf("/");
-					AddToEquation(UnicodeCharacter[0]);
-					return -1;
-
-				case 43: // +
-					printf("+");
-					AddToEquation(UnicodeCharacter[0]);
-					return -1;
-
-				case 45: // -
-					printf("-");
-					AddToEquation(UnicodeCharacter[0]);
-					return -1;
-
-				case 42: // *
-					printf("*");
-					AddToEquation(UnicodeCharacter[0]);
-					return -1;
-
-				case 46: // .
-					printf(".");
-					AddToEquation(UnicodeCharacter[0]);
-					return -1;
-
-				case 13: // {ENTER}
-				case 61: // =
-					Enabled = false; // "disable" hook
-
-					clear_screen();
-
-					double EquationDoubleOutput = te_interp(Equation, 0); // intepret and solve the equation
-
-					std::cout << Equation << " = " << EquationDoubleOutput << "\n"; // display full equation with answer
-
-					// clear Equation array and zero Array Index Pointer
-					std::fill(std::begin(Equation), std::end(Equation), 0);
-					EquationArrayIndexPointer = 0;
-
-					// convert Equation Answer to string with removing trailing 0s
-					std::string EquationOutput = std::to_string(EquationDoubleOutput);
-					EquationOutput.erase(EquationOutput.find_last_not_of('0') + 1, std::string::npos);
-					EquationOutput.erase(EquationOutput.find_last_not_of('.') + 1, std::string::npos);
-
-					// for loop for each character in equation answer and simulate keyboard event
-					for(char ch : EquationOutput)
-					{
-						keybd_event(VkKeyScanExA(ch, GetKeyboardLayout(0)), (UINT)kbdStruct.scanCode, 0, 0);
-						keybd_event(VkKeyScanExA(ch, GetKeyboardLayout(0)), (UINT)kbdStruct.scanCode, KEYEVENTF_KEYUP,0);
-					}
-
-					return -1;
+					NewCoord = {(SHORT)(GetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE)).X - 1), GetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE)).Y}; // create new coord with x-1 and same y
+					SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), NewCoord); // use new coord
+					printf(" "); // delete character
+					SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), NewCoord); // go back 1 space
+					EquationArrayIndexPointer--;
 				}
+
+				Equation[EquationArrayIndexPointer] = NULL;
+				return -1;
+
+			case 47: // /
+				printf("/");
+				AddToEquation(UnicodeCharacter[0]);
+				return -1;
+
+			case 43: // +
+				printf("+");
+				AddToEquation(UnicodeCharacter[0]);
+				return -1;
+
+			case 45: // -
+				printf("-");
+				AddToEquation(UnicodeCharacter[0]);
+				return -1;
+
+			case 42: // *
+				printf("*");
+				AddToEquation(UnicodeCharacter[0]);
+				return -1;
+
+			case 46: // .
+				printf(".");
+				AddToEquation(UnicodeCharacter[0]);
+				return -1;
+
+			case 13: // {ENTER}
+			case 61: // =
+				EnabledEquation = false; // "disable" hook
+
+				clear_screen();
+
+				double EquationDoubleOutput = te_interp(Equation, 0); // intepret and solve the equation
+
+				std::cout << Equation << " = " << EquationDoubleOutput << "\n"; // display full equation with answer
+
+				// clear Equation array and zero Array Index Pointer
+				std::fill(std::begin(Equation), std::end(Equation), 0);
+				EquationArrayIndexPointer = 0;
+
+				// convert Equation Answer to string with removing trailing 0s
+				std::string EquationOutput = std::to_string(EquationDoubleOutput);
+				EquationOutput.erase(EquationOutput.find_last_not_of('0') + 1, std::string::npos);
+				EquationOutput.erase(EquationOutput.find_last_not_of('.') + 1, std::string::npos);
+
+				// for loop for each character in equation answer and simulate keyboard event
+				for(char ch : EquationOutput)
+				{
+					keybd_event(VkKeyScanExA(ch, GetKeyboardLayout(0)), (UINT)kbdStruct.scanCode, 0, 0);
+					keybd_event(VkKeyScanExA(ch, GetKeyboardLayout(0)), (UINT)kbdStruct.scanCode, KEYEVENTF_KEYUP,0);
+				}
+
+				return -1;
 			}
+		}
+	}
+	else if (EnableAutoSelect)
+	{
+		switch (wParam)
+		{
+		// if keydown hook event (allows for holding and "spamming" key)
+		case WM_KEYDOWN:
+			// Pointer to UnicodeCharacter Array
+			wchar_t* UnicodeCharacter;
+			UnicodeCharacter = GetUnicodeCharacter(lParam);
+
+			switch (UnicodeCharacter[0])
+			{
+			case 48: // 0
+			case 49: // 1
+			case 50: // 2
+			case 51: // 3
+			case 52: // 4
+			case 53: // 5
+			case 54: // 6
+			case 55: // 7
+			case 56: // 8
+			case 57: // 9
+				std::wcout << UnicodeCharacter;
+				//AddToArray(UnicodeCharacter[0]);
+				return -1;
+
+			case 13: // {ENTER}
+				return -1;
+			}
+
+			break;
 		}
 	}
 
@@ -297,9 +346,9 @@ int main()
 			switch (msg.wParam)
 			{
 			case EquationEnableHK:
-				Enabled = !Enabled;
+				EnabledEquation = !EnabledEquation;
 
-				if (Enabled)
+				if (EnabledEquation)
 				{
 					printf("Enabled\n");
 				}
@@ -310,6 +359,12 @@ int main()
 				break;
 
 			case AutoSelectHK:
+				//initiate Auto Select by asking amount
+				printf("Input amount to go right by: ");
+
+				AutoSelectFirstPass = true;
+				EnableAutoSelect = true;
+
 				//std::async(std::launch::async, AutoSelect);
 				AutoSelect();
 				break;
